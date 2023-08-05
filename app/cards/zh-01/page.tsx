d90a56e3ec61eb98e29d498065dc12065b0f5af1
Ming Jun <evan.zhangmingjun@gmail.com>
2023-08-06 01:19:52 +0800

    completed cards

--- app/cards/zh-01/page.tsx
+++ app/cards/zh-01/page.tsx
@@ -1,45 +1,68 @@
+"use client";
+
 import { ArrowLeft } from "lucide-react";
 
-// export default function Page() {
-//   return (
-//     <>
-//       <div className="bg-neutral-50 min-h-full relative">
-//         <div className="bg-indigo-500 h-1/3 rounded-b-3xl absolute top-0 left-0 z-10"></div>
-//         <div className="flex flex-col bg-neutral-50 p-12">
-//           <ArrowLeft />
-//           <h1>中学普通华文</h1>
-//           <div className="bg-white">
-//             <h1 className="text-5xl">神秘</h1>
-//           </div>
-//           <button className="bg-indigo-500 text-white">FLIP CARD</button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 export default function Page() {
+  function handleClick() {
+    document.querySelector(".card-back")?.classList.toggle("invisible");
+    document.querySelector(".flip-button")?.classList.toggle("invisible");
+    document.querySelector(".rate-button")?.classList.toggle("invisible");
+  }
+
   return (
     <>
-      {/* <div className="bg-neutral-500 h-12 relative"></div>
-      <div className="bg-indigo-500 absolute top-0 left-0 z-10 h-6"></div> */}
       <div className="relative h-full w-full">
         <div className="bg-neutral-50 h-full" />
         <div className="absolute top-0 left-0 bg-indigo-500 h-2/5 w-full rounded-b-3xl" />
         <div className="flex flex-col gap-8 absolute top-0 left-0 p-12 w-full h-full">
           <ArrowLeft color="white" size={48} />
           <h1 className="text-3xl text-white">中学普通华文</h1>
-          <div className="flex flex-grow">
-            <div className="front flex flex-col bg-white flex-grow rounded-2xl shadow place-content-center absolute top-0 left-0">
+          <div className="flex flex-grow relative">
+            <div className="card-front flex flex-col bg-white flex-grow h-full w-full rounded-2xl shadow place-content-center absolute top-0 left-0 visible">
               <h1 className="text-7xl text-center font-bold">神秘</h1>
             </div>
-            <div className="back flex flex-col bg-white flex-grow rounded-2xl shadow place-content-center absolute top-0 left-0">
+            <div className="card-back flex flex-col gap-4 bg-white flex-grow h-full w-full p-8 rounded-2xl shadow place-content-center absolute top-0 left-0 invisible">
               <h1 className="text-7xl text-center font-bold">神秘</h1>
+              <h3 className="text-3xl text-center text-neutral-700">shén mì</h3>
+              <hr />
+              <h3 className="text-3xl text-center text-neutral-700">
+                mysterious
+              </h3>
+              <hr />
+              <div>
+                <p className="mb-1">Example:</p>
+                <span>
+                  他喜欢阅读关于宇宙的书籍，以此来探索那个<b>神秘</b>的世界
+                </span>
+              </div>
             </div>
           </div>
-          <button className="bg-indigo-500 text-white p-4 rounded-2xl">
-            FLIP CARD
-          </button>
+          <div className="h-16 relative">
+            <button
+              className="flip-button bg-indigo-500 text-white p-4 rounded-2xl w-full h-full absolute top-0 left-0"
+              onClick={handleClick}
+            >
+              FLIP CARD
+            </button>
+            <div className="rate-button flex flex-row bg-white rounded-2xl w-full h-full absolute top-0 left-0 shadow invisible overflow-clip">
+              <div className="flex flex-col grow border-r">
+                <div className="bg-red-500 w-full h-2"></div>
+                <button className="grow">AGAIN</button>
+              </div>
+              <div className="flex flex-col grow border-r">
+                <div className="bg-yellow-500 w-full h-2"></div>
+                <button className="grow">HARD</button>
+              </div>
+              <div className="flex flex-col grow border-r">
+                <div className="bg-blue-500 w-full h-2"></div>
+                <button className="grow">GOOD</button>
+              </div>
+              <div className="flex flex-col grow border-r">
+                <div className="bg-green-500 w-full h-2"></div>
+                <button className="grow">EASY</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>

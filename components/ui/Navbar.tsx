1bc3bfa0bdd2dad233f171df8a98caa8e41ab554
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-08-05 23:07:01 +0800

    added all three icons to navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
 import quizIcon from '../../public/svg/quiz_FILL0_wght400_GRAD0_opsz48.svg'
+import settingsIcon from '../../public/svg/settings_FILL0_wght400_GRAD0_opsz48.svg'
+import cardsIcon from '../../public/svg/style_FILL0_wght400_GRAD0_opsz48 (1).svg'
+
+let iconStyle = "h-8"
 
 export default function Navbar() {
-    return <footer className="flex flex-row justify-evenly bg-gray-500 h-16">
+    return <footer className="flex flex-row justify-evenly items-center  h-16
+    shadow shadow-slate-700">
         <Link href="/quiz">
-            <Image alt="nav-item quiz" src={quizIcon} className="fill-white stroke-white text-white"/>
+            <Image alt="nav-item quiz" src={quizIcon} className={iconStyle} />
         </Link>
-
+        <Link href="/">
+            <Image alt="nav-item quiz" src={cardsIcon} className={iconStyle} />
+        </Link>
+        <div >
+            <Image alt="nav-item settings" src={settingsIcon} className={iconStyle} />
+        </div>
     </footer>
 }

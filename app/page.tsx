db62acc5a2f34e47f7937086ecf614411ae5541e
Kuang Yu Heng <yuheng3107@gmail.com>
2023-08-06 10:47:30 +0800

    Added button

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
           <span className="text-6xl font-bold">69</span>
           <span>days</span>
         </div>
-
       </div>
       <section className="w-full flex flex-col justify-start p-4 gap-y-4">
         <div className="flex flex-row justify-between">
@@ -24,8 +23,10 @@ export default function Home() {
           </button>
         </div>
         <ListItem text="中学华文" time="2days ago" icon="bg-amber-200" />
+        <div className="flex justify-center">
+          <button className="p-2 border-2 w-1/3 rounded-sm">Add Deck +</button>
+        </div>
       </section>
-
     </main>
   );
 }

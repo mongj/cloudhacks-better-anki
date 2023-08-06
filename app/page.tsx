3c550142a32aa469052252430ba430468a244ae7
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-08-06 10:42:43 +0800

    routed app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 import McqForm from "../components/mcqform";
 import tests from "../public/data/tests.json";
+import Image from "next/image";
+import ListItem from "../components/ui/ListItem";
+import { ChevronDown } from "lucide-react";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <McqForm
-        word={tests[0].word}
-        false_options={tests[0].false_options}
-      ></McqForm>
+      <div className="h-40 w-full bg-cyan-600 text-white flex flex-col justify-center items-center">
+        streak
+        <div>
+          <span className="text-6xl font-bold">69</span>
+          <span>days</span>
+        </div>
+
+      </div>
+      <section className="w-full flex flex-col justify-start p-4 gap-y-4">
+        <div className="flex flex-row justify-between">
+          <span className="font-semibold text-lg ml-2">Quizzes</span>
+          <button className="flex flex-row justify-center items-center">
+            <span className="text-stone-500">last reviewed</span>
+            <ChevronDown className="w-6 h-6" />
+          </button>
+        </div>
+        <ListItem text="中学华文" time="2days ago" icon="bg-amber-200" />
+      </section>
+
     </main>
   );
 }
